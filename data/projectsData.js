9d1ba4f05aa3bb84882b4ca372b3ae45b0aba555
Fortan Pireva <fortanpireva99@gmail.com>
2023-01-29 18:50:45 +0100

    add custom projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,23 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'A basic social media posting site',
+    description: `A basic video and photo upload React + Firebase project leveraging Cloud Firestore and Cloud Storage 
+    for data, photo and video storage`,
+    imgSrc: '/static/images/projects/slanconnect.png',
+    href: 'https://slanconnect.vercel.app',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'SlinggyApps',
+    description: `Landing page created in Next.JS showcasing a  organization building in the shopify ecosystem.`,
+    imgSrc: '/static/images/projects/slinggyapps.png',
+    href: 'https://slinggyapps.com',
+  },
+  {
+    title: 'Foodpiry',
+    description:
+      'A near-expiry food search database with notification feature implemented when new products are about to expire \n.Implemented in React.JS and Firebase',
+    imgSrc: '/static/images/projects/foodpiry.png',
+    href: 'https://foodsearch-927fe.web.app/',
   },
 ]
 
--- pages/projects.js
+++ pages/projects.js
@@ -13,7 +13,8 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Some side projects implemented in my free time, mostly leveragin React.JS, Typescript,
+            Firebase and Next.JS
           </p>
         </div>
         <div className="container py-12">
