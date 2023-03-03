4123adc49a07db96f96c9687c5ee4525ad7f9c15
Fortan Pireva <fortanpireva99@gmail.com>
2023-03-03 14:31:06 +0100

    change img seo

--- components/SEO.js
+++ components/SEO.js
@@ -15,9 +15,9 @@ const CommonSEO = ({ title, description, ogType, ogImage, twImage, canonicalUrl 
       <meta property="og:description" content={description} />
       <meta property="og:title" content={title} />
       {ogImage.constructor.name === 'Array' ? (
-        ogImage.map(({ url }) => <meta property="og:image" content={url} key={url} />)
+        ogImage.map(({ url }) => <meta property="og:image:url" content={url} key={url} />)
       ) : (
-        <meta property="og:image" content={ogImage} key={ogImage} />
+        <meta property="og:image:url" content={ogImage} key={ogImage} />
       )}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content={siteMetadata.twitter} />
